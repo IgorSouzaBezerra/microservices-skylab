3d41b7494957479b47e9d13e04423617ae5a1189
Igor de Souza Bezerra <igor.souza1.bezerra@gmail.com>
2022-04-26 11:35:32 -0300

    first commit

---
+++ gateway/src/app.module.ts
@@ -0,0 +1,19 @@
+import { IntrospectAndCompose } from '@apollo/gateway';
+import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloGatewayDriverConfig>({
+      driver: ApolloGatewayDriver,
+      server: {
+        cors: true,
+      },
+      gateway: {
+        supergraphSdl: new IntrospectAndCompose(),
+      },
+    }),
+  ],
+})
+export class AppModule {}
